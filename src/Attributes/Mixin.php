2e0d296ce0dbaf0805b3bd47aa89355ce67a5c60
Sven Luijten <svenluijten1995@gmail.com>
2021-04-20 22:12:24 +0200

    Make annotation classes final

--- src/Attributes/Mixin.php
+++ src/Attributes/Mixin.php
@@ -6,7 +6,7 @@ use Attribute;
 use Illuminate\Support\Traits\Macroable;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-class Mixin
+final class Mixin
 {
     public function __construct(public Macroable $target, public $replace = true)
     {
